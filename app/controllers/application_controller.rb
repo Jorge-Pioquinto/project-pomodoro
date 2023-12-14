f755714af964420369ed72d7508dbb48d3ac4f3f
Jorge-Pioquinto <jlpioquinto@gmail.com>
2023-12-14 13:33:05 -0600

    Added redirect for signed-in users on root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :redirect_if_signed_in
+
+  private
+
+  def redirect_if_signed_in
+    if user_signed_in? && request.path == root_path
+      redirect_to tasks_path
+    end
+  end
 end
